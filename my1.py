7bffe30ce3ff8db2a5545abc41581c25ae51c1d8
Alina-2109 <74093317+Alina-2109@users.noreply.github.com>
2020-11-19 20:31:17 +0300

    Add files via upload

---
+++ my1.py
@@ -0,0 +1,21 @@
+print("Hello Country") #Приветствие
+MyName = " Алина " #Имя
+myAge = " 17" #Возвраст
+mySurname = " Лазарева " #Фамилия
+MyCity = " Окуловка " #Родной город
+WhereILive = " Великом Новгороде " #Место жительства
+iStudy = "НСК" #место получения образования
+mySpecialty = "информационные системы по отрослям" #специальность
+LittleAboutYourself = " о себе:не пью, не курю, не влюбляюсь. берегу печень, лёгкие и нервы "
+print(MyName, mySurname, myAge, MyCity, WhereILive, iStudy, mySpecialty, LittleAboutYourself)
+WhatIDo = " в свободное время залипаю в телефоне " #Любимое занятие
+andalso = " Кстати мне осталось учиться "
+a = 3.10 #всего учится
+b = 1.02 #уже отучился
+c = a - b #осталось
+с = 2.08
+print(WhatIDo)
+isTrue = True
+print(andalso, c, isTrue)
+Theand = " На сегодня хватит,хорошего вечера :)"
+print(Theand)
